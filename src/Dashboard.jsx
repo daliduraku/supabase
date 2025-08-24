b795d57d34408fb0c2d520d3dd7355bd32a1a41c
Dali Duraku <mesherbetmolla@gmail.com>
2025-08-24 15:53:39 -0400

    feat: react charts

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,19 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import supabase from "./supabase-client";
 
 export default function Dashboard() {
+  const [metrics, setMetrics] = useState([]);
+
   useEffect(() => {
     async function fetchMetrics() {
-      const { data, error } = await supabase
-        .from("sales_deals")
-        .select(
-          `
-          name,
-          value.sum()
-          `
-        )
-        .order("value", { ascending: false })
-        .limit(1);
+      try {
+        const { data, error } = await supabase
+          .from("sales_deals")
+          .select(
+            `
+            name,
+            value.sum()
+            `
+          )
+          .order("value", { ascending: false })
+          .limit(1);
+        if (error) {
+          throw error;
+        }
+        console.log(data);
+        setMetrics(data);
+        console.log(error);
+      } catch (error) {
+        console.error("Caught an error:", error);
+      }
     }
 
     fetchMetrics();
