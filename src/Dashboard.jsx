269886b42fb54a3d24196de0eddde765ea8fd5d3
Dali Duraku <mesherbetmolla@gmail.com>
2025-08-24 16:10:46 -0400

    feat: format date for chart

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import supabase from "./supabase-client";
+import { Chart } from "react-charts";
 
 export default function Dashboard() {
   const [metrics, setMetrics] = useState([]);
@@ -7,16 +8,12 @@ export default function Dashboard() {
   useEffect(() => {
     async function fetchMetrics() {
       try {
-        const { data, error } = await supabase
-          .from("sales_deals")
-          .select(
-            `
+        const { data, error } = await supabase.from("sales_deals").select(
+          `
             name,
             value.sum()
             `
-          )
-          .order("value", { ascending: false })
-          .limit(1);
+        );
         if (error) {
           throw error;
         }
@@ -31,10 +28,61 @@ export default function Dashboard() {
     fetchMetrics();
   }, []);
 
+  const chartData = [
+    {
+      data: metrics.map((m) => ({
+        primary: m.name,
+        secondary: m.sum,
+      })),
+    },
+  ];
+
+  const primaryAxis = {
+    getValue: (d) => d.primary,
+    scaleType: "band",
+    padding: 0.2,
+    position: "bottom",
+  };
+
+  const secondaryAxes = [
+    {
+      getValue: (d) => d.secondary,
+      scaleType: "linear",
+      min: 0,
+      max: y_max(),
+      padding: {
+        top: 20,
+        bottom: 40,
+      },
+    },
+  ];
+
+  function y_max() {
+    if (metrics.length > 0) {
+      const maxSum = Math.max(...metrics.map((m) => m.sum));
+      return maxSum + 2000;
+    }
+    return 5000;
+  }
+
   return (
     <div className="dashboard-wrapper">
       <div className="chart-container">
         <h2>Total Sales This Quarter ($)</h2>
+        <div style={{ flex: 1 }}>
+          <Chart
+            options={{
+              data: chartData,
+              primaryAxis,
+              secondaryAxes,
+              type: "bar",
+              defaultColors: ["#58d675"],
+              tooltip: {
+                show: false,
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
