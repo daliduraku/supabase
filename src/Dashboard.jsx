67fd40c973994c31e79ef4d3d68beb7a030238e7
Dali Duraku <mesherbetmolla@gmail.com>
2025-08-24 15:08:38 -0400

    feat: fetch data

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+import supabase from "./supabase-client";
+
 export default function Dashboard() {
+  useEffect(() => {
+    async function fetchMetrics() {
+      const { data, error } = await supabase
+        .from("sales_deals")
+        .select(
+          `
+          name,
+          value.sum()
+          `
+        )
+        .order("value", { ascending: false })
+        .limit(1);
+    }
+
+    fetchMetrics();
+  }, []);
+
   return (
     <div className="dashboard-wrapper">
       <div className="chart-container">
