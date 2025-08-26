4f8605f46f45e8066df583b1b43a5065bb10b3b2
Dali Duraku <mesherbetmolla@gmail.com>
2025-08-25 22:50:32 -0400

    feat: sign in auth function

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -32,6 +32,27 @@ export const AuthContextProvider = ({ children }) => {
     }
   }
   // Auth functions (signin, signup, logout)
+  const signInUser = async (email, password) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email.toLowerCase(),
+        password: password,
+      });
+      if (error) {
+        console.error("Supabase sign-in error:", error.message);
+        return { success: false, error: error.message };
+      }
+      console.log("Supabase sign-in success:", data);
+      return { success: true, data };
+    } catch (error) {
+      //Unexpected error
+      console.error("Unexpected error during sign-in:", error.message);
+      return {
+        success: false,
+        error: "An unexpected error occurred. Please try again.",
+      };
+    }
+  };
 
   return (
     <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
